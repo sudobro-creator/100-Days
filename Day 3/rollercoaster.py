dee219cce4d10ef61a977f257462c04dfd5b703a
Sudo Bro <112646122+sudobro-creator@users.noreply.github.com>
2025-05-05 18:01:43 +0100

    Update rollercoaster.py
    added bill and pictures functionality.

--- Day 3/rollercoaster.py
+++ Day 3/rollercoaster.py
@@ -1,14 +1,24 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print('You can ride.')
     age = int(input("How are old are you?"))
     if age <= 12:
-        print('You have to pay $5.')
+        bill = 5
+        print('Child Ticket is $5.')
     elif age <= 18:
-        print("You have to pay $7.") 
+        bill = 7
+        print("Teen Ticket is $7.") 
     else:
-        print("Pay $12.")   
+        bill = 12
+        print("Adult Ticket is $12.")
+    
+    photo_bill = input('Do you want to have a picture? Type y for Yes and n for No.')
+    if photo_bill == 'y':
+        bill += 3
+        
+    print(f'Your final bill is ${bill}.')
 else:
     print('You need to go taller to ride the rollercoaster.')
