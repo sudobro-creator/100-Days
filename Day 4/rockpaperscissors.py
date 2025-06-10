5c1333da1f36de56547d961254767890bbb74140
Sudo Bro <112646122+sudobro-creator@users.noreply.github.com>
2025-06-10 22:44:04 +0100

    updated rockpaperscissors.py

--- Day 4/rockpaperscissors.py
+++ Day 4/rockpaperscissors.py
@@ -43,26 +43,30 @@ rock = 0
 paper = 1
 scissor = 2
 
-computer = [0, 1, 2]
-computer_choice = random.choice(computer)
-user_choice = int(input("Select one number between 0 to 2 to select your option: "))
-if user_choice >= 0 and user_choice <= 2:
-    print(game_images[user_choice])
+while True:
+    computer = [0, 1, 2]
+    computer_choice = random.choice(computer)
+    user_choice = int(input("Select one number between 0 to 2 to select your option: "))
+    if user_choice >= 0 and user_choice <= 2:
+        print(game_images[user_choice])
 
-if user_choice == 0 and computer_choice == 2:
-    print("You Win. You picked rock and you broke computer scissors.")
-elif user_choice == 1 and computer_choice == 0:
-    print("You win. You picked paper and wrapped computer rock.")
-elif user_choice == 2 and computer_choice == 1:
-    print("You win. You picked Scissors and you ripped computer paper.")
-elif user_choice == computer_choice:
-    print("It is a draw.")
-elif user_choice != 0 or 1 or 2:
-    print("You typed an invalid number. You lose.")
-else:
-    print("You lose. Computer pick:")
-    print(game_images[computer_choice])
+    if user_choice == 0 and computer_choice == 2:
+        print("You Win. You picked rock and you broke computer scissors.")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You win. You picked paper and wrapped computer rock.")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You win. You picked Scissors and you ripped computer paper.")
+    elif user_choice == computer_choice:
+        print("It is a draw.")
+    elif user_choice != 0 or 1 or 2:
+        print("You typed an invalid number. You lose.")
+    else:
+        print("You lose. Computer pick:")
+        print(game_images[computer_choice])
 
-
+    restart = input("Do you want to play again? Type 'yes' to continue or 'no' to exit: ").lower()
+    if restart != 'yes':
+        print("Thank you for playing. Goodbye!")
+        break
 
 print(user_choice)
