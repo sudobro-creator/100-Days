e6d4d41c4377a5a69f15261fad783be148b4d971
Sudo Bro <112646122+sudobro-creator@users.noreply.github.com>
2025-06-10 23:13:03 +0100

    update hangman.py

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -56,11 +56,238 @@ stages = [r'''
 =========
 ''']
 
-word_list = ["ronaldo", "messi", "kaka", "neymar", "pele", "maradona", "zidane", "maldini", "buffon", "xavi", "iniesta"]
+logo = r''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
 
+print(logo)
+
+
+word_list = [
+    'abruptly',
+    'absurd',
+    'abyss',
+    'affix',
+    'askew',
+    'avenue',
+    'awkward',
+    'axiom',
+    'azure',
+    'bagpipes',
+    'bandwagon',
+    'banjo',
+    'bayou',
+    'beekeeper',
+    'bikini',
+    'blitz',
+    'blizzard',
+    'boggle',
+    'bookworm',
+    'boxcar',
+    'boxful',
+    'buckaroo',
+    'buffalo',
+    'buffoon',
+    'buxom',
+    'buzzard',
+    'buzzing',
+    'buzzwords',
+    'caliph',
+    'cobweb',
+    'cockiness',
+    'croquet',
+    'crypt',
+    'curacao',
+    'cycle',
+    'daiquiri',
+    'dirndl',
+    'disavow',
+    'dizzying',
+    'duplex',
+    'dwarves',
+    'embezzle',
+    'equip',
+    'espionage',
+    'euouae',
+    'exodus',
+    'faking',
+    'fishhook',
+    'fixable',
+    'fjord',
+    'flapjack',
+    'flopping',
+    'fluffiness',
+    'flyby',
+    'foxglove',
+    'frazzled',
+    'frizzled',
+    'fuchsia',
+    'funny',
+    'gabby',
+    'galaxy',
+    'galvanize',
+    'gazebo',
+    'giaour',
+    'gizmo',
+    'glowworm',
+    'glyph',
+    'gnarly',
+    'gnostic',
+    'gossip',
+    'grogginess',
+    'haiku',
+    'haphazard',
+    'hyphen',
+    'iatrogenic',
+    'icebox',
+    'injury',
+    'ivory',
+    'ivy',
+    'jackpot',
+    'jaundice',
+    'jawbreaker',
+    'jaywalk',
+    'jazziest',
+    'jazzy',
+    'jelly',
+    'jigsaw',
+    'jinx',
+    'jiujitsu',
+    'jockey',
+    'jogging',
+    'joking',
+    'jovial',
+    'joyful',
+    'juicy',
+    'jukebox',
+    'jumbo',
+    'kayak',
+    'kazoo',
+    'keyhole',
+    'khaki',
+    'kilobyte',
+    'kiosk',
+    'kitsch',
+    'kiwifruit',
+    'klutz',
+    'knapsack',
+    'larynx',
+    'lengths',
+    'lucky',
+    'luxury',
+    'lymph',
+    'marquis',
+    'matrix',
+    'megahertz',
+    'microwave',
+    'mnemonic',
+    'mystify',
+    'naphtha',
+    'nightclub',
+    'nowadays',
+    'numbskull',
+    'nymph',
+    'onyx',
+    'ovary',
+    'oxidize',
+    'oxygen',
+    'pajama',
+    'peekaboo',
+    'phlegm',
+    'pixel',
+    'pizazz',
+    'pneumonia',
+    'polka',
+    'pshaw',
+    'psyche',
+    'puppy',
+    'puzzling',
+    'quartz',
+    'queue',
+    'quips',
+    'quixotic',
+    'quiz',
+    'quizzes',
+    'quorum',
+    'razzmatazz',
+    'rhubarb',
+    'rhythm',
+    'rickshaw',
+    'schnapps',
+    'scratch',
+    'shiv',
+    'snazzy',
+    'sphinx',
+    'spritz',
+    'squawk',
+    'staff',
+    'strength',
+    'strengths',
+    'stretch',
+    'stronghold',
+    'stymied',
+    'subway',
+    'swivel',
+    'syndrome',
+    'thriftless',
+    'thumbscrew',
+    'topaz',
+    'transcript',
+    'transgress',
+    'transplant',
+    'triphthong',
+    'twelfth',
+    'twelfths',
+    'unknown',
+    'unworthy',
+    'unzip',
+    'uptown',
+    'vaporize',
+    'vixen',
+    'vodka',
+    'voodoo',
+    'vortex',
+    'voyeurism',
+    'walkway',
+    'waltz',
+    'wave',
+    'wavy',
+    'waxy',
+    'wellspring',
+    'wheezy',
+    'whiskey',
+    'whizzing',
+    'whomever',
+    'wimpy',
+    'witchcraft',
+    'wizard',
+    'woozy',
+    'wristwatch',
+    'wyvern',
+    'xylophone',
+    'yachtsman',
+    'yippee',
+    'yoked',
+    'youthful',
+    'yummy',
+    'zephyr',
+    'zigzag',
+    'zigzagging',
+    'zilch',
+    'zipper',
+    'zodiac',
+    'zombie',
+]
 
 lives = 6
-print("Welcome to Sport Hangman!")
+print("Welcome to Hangman!")
+print("****************************<???>/6 LIVES LEFT****************************")
 chosen_word = random.choice(word_list)
 
 
@@ -68,7 +295,7 @@ placeholder = ""
 word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
-print(placeholder)
+print("Word to guess: " + placeholder)
 
 
 game_over = False
@@ -76,9 +303,11 @@ correct_letters = []
 
 while not game_over:
     guess = input("Guess a letter: ").lower()
+    
     display = ""
-
-
+    if guess in correct_letters:
+        print(f"You have already guessed the letter '{guess}'. Try again.")
+        continue
     for letter in chosen_word:
         if letter == guess:
             display += letter
@@ -87,19 +316,19 @@ while not game_over:
             display += letter
         else:
             display += "_"
-    print(display)
+    print("Word to guess: " + display)
 
     if guess not in chosen_word:
         lives -= 1
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         if lives == 0:
             game_over = True
-            print("You lose!")
+            print(f"***********************YOU LOSE**********************")
             break
 
     if "_" not in display:
         game_over = True
-        print("You win!")
+        print("****************************YOU WIN****************************")
 
 
     print(stages[lives])
